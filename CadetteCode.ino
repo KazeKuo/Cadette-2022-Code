c760e4c264a20e5358996ee491db3005d5e1fa8a
KazeKuo <88811155+KazeKuo@users.noreply.github.com>
2021-12-04 16:06:44 -0800

    Update CadetteCode.ino

--- CadetteCode.ino
+++ CadetteCode.ino
@@ -1,115 +1,47 @@
 #include <A4990MotorShield.h>
 
-/*
- * This example uses the A4990MotorShield library to drive each motor with the
- * Pololu A4990 Dual Motor Driver Shield for Arduino forward, then backward. 
- * The yellow user LED is on when a motor is set to a positive speed and off when
- * a motor is set to a negative speed.
- */
-
-#define LED_PIN 13
-
 A4990MotorShield motors;
-
-/* 
- * For safety, it is good practice to monitor motor driver faults and handle
- * them in an appropriate way. If a fault is detected, both motor speeds are set
- * to zero and it is reported on the serial port.
- */
-void stopIfFault()
-{
-  if (motors.getFault())
-  {
-    motors.setSpeeds(0,0);
-    Serial.println("Fault");
-    while(1);
-  }
-}
 
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);
-  Serial.begin(115200);
-  Serial.println("Pololu A4990 Dual Motor Driver Shield for Arduino");
-  
-  // uncomment one or both of the following lines if your motors' directions need to be flipped
-  //motors.flipM1(true);
-  //motors.flipM2(true);
+  motors.flipM1(true);
+  motors.flipM2(true);
 }
+
+void goStraight(int M1Speed, int M2Speed, double duration) {
+  motors.setSpeeds(M1Speed, M2Speed);
+  delay(duration);
+  motors.setSpeeds(0,0);
+}
+ 
+void turnClockwise(double duration) {
+  int M1TurnSpeed=100;
+  int M2TurnSpeed=400;
+  motors.setSpeeds(M1TurnSpeed, M2TurnSpeed);
+  delay(duration);
+  motors.setSpeeds(0,0);
+}
+
+void turnCounterClockwise(double duration) {
+  int M1TurnSpeed=400;
+  int M2TurnSpeed=100;
+  motors.setSpeeds(M1TurnSpeed, M2TurnSpeed);
+  delay(duration);
+  motors.setSpeeds(0,0);
+}
+
 
 void loop()
 {
-  // run M1 motor with positive speed
+  int M1speed = 0;
+  int M2speed = 0;
+  int duration = 0;
 
-  digitalWrite(LED_PIN, HIGH);
-  
-  for (int speed = 0; speed <= 400; speed++)
-  {
-    motors.setM1Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
+  goStraight(M1speed,M2speed,duration);
 
-  for (int speed = 400; speed >= 0; speed--)
-  {
-    motors.setM1Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
   
-  // run M1 motor with negative speed
   
-  digitalWrite(LED_PIN, LOW);
-  
-  for (int speed = 0; speed >= -400; speed--)
-  {
-    motors.setM1Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
-  
-  for (int speed = -400; speed <= 0; speed++)
-  {
-    motors.setM1Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
-
-  // run M2 motor with positive speed
-  
-  digitalWrite(LED_PIN, HIGH);
-  
-  for (int speed = 0; speed <= 400; speed++)
-  {
-    motors.setM2Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
-
-  for (int speed = 400; speed >= 0; speed--)
-  {
-    motors.setM2Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
-  
-  // run M2 motor with negative speed
-  
-  digitalWrite(LED_PIN, LOW);
-  
-  for (int speed = 0; speed >= -400; speed--)
-  {
-    motors.setM2Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
-  
-  for (int speed = -400; speed <= 0; speed++)
-  {
-    motors.setM2Speed(speed);
-    stopIfFault();
-    delay(2);
-  }
   
   delay(500);
+  exit(0);
 }
