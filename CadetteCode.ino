70ab9cf82682a2998dc09f6059d06c6b67e97d01
KazeKuo <88811155+KazeKuo@users.noreply.github.com>
2022-03-04 19:07:50 -0800

    Update CadetteCode.ino

--- CadetteCode.ino
+++ CadetteCode.ino
@@ -8,13 +8,13 @@ void setup()
   motors.flipM2(true);
 }
 
-void move(int M1Speed, int M2Speed, double duration) {
+void move(int M1Speed, int M2Speed, int duration) {
   motors.setSpeeds(M1Speed, M2Speed);
   delay(duration);
   motors.setSpeeds(0,0);
 }
  
-void turnClockwise(double duration) {
+void turnClockwise(int duration) {
   int M1TurnSpeed=100;
   int M2TurnSpeed=400;
   motors.setSpeeds(M1TurnSpeed, M2TurnSpeed);
@@ -22,7 +22,7 @@ void turnClockwise(double duration) {
   motors.setSpeeds(0,0);
 }
 
-void turnCounterClockwise(double duration) {
+void turnCounterClockwise(int duration) {
   int M1TurnSpeed=400;
   int M2TurnSpeed=100;
   motors.setSpeeds(M1TurnSpeed, M2TurnSpeed);
